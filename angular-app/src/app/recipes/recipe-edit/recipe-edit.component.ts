cb4d875ea1d46810c0dc0c99e5328580c151ff8c
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-04-04 12:58:35 +0300

    Recipe Form

--- angular-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ angular-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -16,7 +16,7 @@ export class RecipeEditComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
-      console.log(this.editMode);
+      // console.log(this.editMode);
       
     });
   }
