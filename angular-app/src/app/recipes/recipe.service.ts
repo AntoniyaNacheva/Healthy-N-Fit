955bc37b5c3e0d40d7a009d0cda13f5f13ff55cb
AntoniyaNacheva <99907501+AntoniyaNacheva@users.noreply.github.com>
2024-04-09 12:02:55 +0300

    Edit Form

--- angular-app/src/app/recipes/recipe.service.ts
+++ angular-app/src/app/recipes/recipe.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-  recipes: Recipe[] = [
+  private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simple a test',
@@ -33,5 +36,15 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
